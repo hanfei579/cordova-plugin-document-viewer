8299eaaf84a9821ffdb0e8cd36dc80d561199e71
hanfei579 <han.fei.579@163.com>
2017-06-02 09:10:32 +0800

    Create FileProvider.java

---
+++ src/android/de/sitewaerts/cordova/documentviewer/FileProvider.java
@@ -0,0 +1,9 @@
+package de.sitewaerts.cordova.documentviewer;
+
+/**
+ * Created by Administrator on 2017/6/1 0001.
+ */
+
+public class FileProvider  extends android.support.v4.content.FileProvider {
+
+}
